fad7400726387b4cb1ffdeacd88473270adf818a
Ajinkya-commits <ajinkyathoke8585@gmail.com>
2025-12-22 16:29:35 +0530

    Implement Companion Form UI

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -36,7 +36,7 @@ const CompanionList = ({
           {companions?.map(({ id, subject, name, topic, duration }) => (
             <TableRow key={id}>
               <TableCell>
-                <Link href={`/companion/${id}`}>
+                <Link href={`/companions/${id}`}>
                   <div className="flex items-center gap-2">
                     <div
                       className="size-18 flex items-center justify-center rounded-lg max-md:hidden"
